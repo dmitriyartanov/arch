5ad29b7a5910e094a9add390fd5cd81aab2a2e34
Artanov Dmitrii <dartanov@luxoft.com>
2019-10-20 18:53:52 +0600

    packages

--- archuefi3.sh
+++ archuefi3.sh
@@ -20,42 +20,33 @@ xdg-user-dirs-update
 echo '3.3 Установка программ'
 sudo pacman -S firefox firefox-i18n-ru ufw f2fs-tools dosfstools ntfs-3g alsa-lib alsa-utils file-roller p7zip unrar gvfs aspell-ru pulseaudio pavucontrol exfat-utils --noconfirm
 
-echo 'Установить рекомендумые программы?'
-read -p "1 - Да, 0 - Нет: " prog_set
-if [[ $prog_set == 1 ]]; then
-  sudo pacman -S recoll obs-studio doublecmd-gtk2 veracrypt vlc freemind filezilla gimp libreoffice libreoffice-fresh-ru kdenlive audacity screenfetch vlc qbittorrent galculator --noconfirm
-  yay -S cherrytree timeshift flameshot-git xflux sublime-text-dev hunspell-ru pamac-aur megasync --noconfirm 
-elif [[ $prog_set == 0 ]]; then
-  echo 'Установка программ пропущена.'
-fi
+echo '3.4 Установка дополнительных программ'
+sudo pacman -S recoll vlc freemind filezilla gimp libreoffice libreoffice-fresh-ru kdenlive audacity screenfetch qbittorrent galculator --noconfirm
+yay -S flameshot-git redshift sublime-text-dev hunspell-ru --noconfirm
 
-echo 'Скачать и установить конфиг и темы для XFCE?'
-read -p "1 - Да, 0 - Нет: " xfce_set
-if [[ $xfce_set == 1 ]]; then
-  echo 'Качаем и устанавливаем настройки Xfce'
-  # Чтобы сделать копию ваших настоек XFCE перейдите в домашнюю директорию ~/username открйте в этой категории терминал и выполните команду ниже.
-  # tar -czf xfce4.tar.gz .config/xfce4
-  # Выгрузите архив в интернет и скорректируйте ссылку на XFCE файл заменив ссылку на свою.
-  # wget git.io/xfce4.tar.gz
-  wget https://github.com/ordanax/arch/raw/master/attach/xfce4.tar.gz
-  sudo rm -rf ~/.config/xfce4/panel/
-  sudo rm -rf ~/.config/xfce4/*
-  sudo tar -xzf xfce4.tar.gz -C ~/
-  echo 'Установка тем'
-  yay -S osx-arc-shadow papirus-maia-icon-theme-git breeze-default-cursor-theme --noconfirm
-  sudo pacman -S capitaine-cursors
-  
-  echo 'Ставим лого ArchLinux в меню'
-  wget git.io/arch_logo.png
-  sudo mv -f ~/builds/arch_logo.png /usr/share/pixmaps/arch_logo.png
 
-  echo 'Ставим обои на рабочий стол'
-  wget git.io/bg.jpg
-  sudo rm -rf /usr/share/backgrounds/xfce/* #Удаляем стандартрые обои
-  sudo mv -f ~/builds/bg.jpg /usr/share/backgrounds/xfce/bg.jpg
-elif [[ $xfce_set == 0 ]]; then
-  echo 'Установка конфигов XFCE пропущена.'
-fi 
+echo '3.5 Качаем и устанавливаем настройки Xfce'
+# Чтобы сделать копию ваших настоек XFCE перейдите в домашнюю директорию ~/username открйте в этой категории терминал и выполните команду ниже.
+# tar -czf xfce4.tar.gz .config/xfce4
+# Выгрузите архив в интернет и скорректируйте ссылку на XFCE файл заменив ссылку на свою.
+# wget git.io/xfce4.tar.gz
+wget https://github.com/ordanax/arch/raw/master/attach/xfce4.tar.gz
+sudo rm -rf ~/.config/xfce4/panel/
+sudo rm -rf ~/.config/xfce4/*
+sudo tar -xzf xfce4.tar.gz -C ~/
+echo 'Установка тем'
+yay -S osx-arc-shadow papirus-maia-icon-theme-git breeze-default-cursor-theme --noconfirm
+sudo pacman -S capitaine-cursors
+
+echo 'Ставим лого ArchLinux в меню'
+wget git.io/arch_logo.png
+sudo mv -f ~/builds/arch_logo.png /usr/share/pixmaps/arch_logo.png
+
+echo 'Ставим обои на рабочий стол'
+wget git.io/bg.jpg
+sudo rm -rf /usr/share/backgrounds/xfce/* #Удаляем стандартрые обои
+sudo mv -f ~/builds/bg.jpg /usr/share/backgrounds/xfce/bg.jpg
+
   
 echo 'Убираем меню граб для выбора системы?'
 read -p "1 - Да, 0 - Нет: " grub_set
