b8e7100d29c014ec1f660a6846956baa5c0e2b5a
dmitriyartanov <dmitryartanov@gmail.com>
2019-11-06 11:46:23 +0600

    usbutils

--- archuefi3.sh
+++ archuefi3.sh
@@ -20,7 +20,7 @@ echo '3.3 Установка программ'
 sudo pacman -S firefox firefox-i18n-ru ufw f2fs-tools dosfstools ntfs-3g alsa-lib alsa-utils file-roller p7zip unrar gvfs aspell-ru pulseaudio pavucontrol exfat-utils redshift --noconfirm
 
 echo '3.4 Установка дополнительных программ'
-sudo pacman -S recoll vlc smplayer freemind filezilla gimp gimp-nufraw geeqie libreoffice libreoffice-fresh-ru kdenlive audacity screenfetch transmission-gtk galculator klavaro calibre solaar bash-completion gnome-keyring seahorse mc breeze-gtk --noconfirm
+sudo pacman -S recoll vlc smplayer freemind filezilla gimp gimp-nufraw geeqie libreoffice libreoffice-fresh-ru kdenlive audacity screenfetch transmission-gtk galculator klavaro calibre solaar bash-completion gnome-keyring seahorse mc breeze-gtk usbutils --noconfirm
 yay -S flameshot-git sublime-text-dev hunspell-ru google-chrome skypeforlinux-stable-bin --noconfirm
 
 echo 'Включаем сетевой экран'
